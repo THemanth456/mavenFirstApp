f3c76c9a3e547a5234e1cb86339fcf8e60c1bb8c
THemanth456 <hemanth.tirumanedi456@gmail.com>
2023-01-23 17:57:57 +0530

    first commit

---
+++ src/main/java/org/LGsoft/mavenproject/mavennewproj/App.java
@@ -0,0 +1,13 @@
+package org.LGsoft.mavenproject.mavennewproj;
+
+/**
+ * Hello world! 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is my new project" );
+    }
+}
